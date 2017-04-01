ccb835d80101ab67ede56d0efd0cae15b2e3751c
Chris liu <gwarmdll@gmail.com>
2017-04-01 22:24:37 +0800

    添加vue 样式

---
+++ src/components/fronted/Front.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div>
+    <vhead></vhead>
+    <section id="main">
+      <router-view></router-view>
+    </section>
+    <ApolloFooter></ApolloFooter>
+  </div>
+</template>
+
+<script>
+  import vhead from './vheader'
+  import ApolloFooter from './ApolloFooter'
+  export default {
+    data () {
+      return {
+        items: [
+        ],
+        loading2: false
+      }
+    },
+    methods: {
+    },
+    components: {
+      vhead,
+      ApolloFooter
+    }
+  }
+</script>
+
+<style lang="css" type="text/css" scoped>
+  @import "../../assets/css/main.css";
+</style>
